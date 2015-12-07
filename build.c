8d9c4b6acae0040dcbc848335f11ecd4f1992adb
Kim A. Jakobsen <kimajakobsen@gmail.com>
2015-12-07 08:12:31 +0100

    reversed the lastdayofthemonth boolean and spelled august correct.

--- build.c
+++ build.c
@@ -671,7 +671,7 @@ holiday holidays[]={
 };
 
 char * month_names[]={"January","February","March","April",
-                 "May","June","July","Augest",
+                 "May","June","July","August",
                  "September","Octorber","November","December"};
 
 char * weekday_names[]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
@@ -715,9 +715,9 @@ mk_date(long index,date_t *d)
     d->lastdayinweekfl[1]='\0';
 
     if(is_last_day_in_month(d->year,d->monthnuminyear,d->daynuminmonth)==1){
-	d->lastdayinmonthfl[0]= '0';
-    }else{
 	d->lastdayinmonthfl[0]= '1';
+    }else{
+	d->lastdayinmonthfl[0]= '0';
     }
     d->lastdayinmonthfl[1]='\0';
  
