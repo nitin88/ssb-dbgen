a590f3eb0c41839e62caec4ae4d5388189a80ad7
Eyal Rozenberg <eyalroz@technion.ac.il>
2017-06-17 15:51:07 +0200

    Merge changes from ZetsubouClown/ssb-dbgen; References issue #1, Fixes issue #5, References issue #4
    * Makefile MACHINE type changed from MAC to LINUX (we'll eventually drop this entirely, but for now let's go with herd on this point)
    * No longer using O_CREAT when opening FIFOs.

--- bm_utils.c
+++ bm_utils.c
@@ -384,7 +384,7 @@ tbl_open(int tbl, char *mode)
     if (S_ISFIFO(fstats.st_mode))
         {
         retcode =
-            open(fullpath, ((*mode == 'r')?O_RDONLY:O_WRONLY)|O_CREAT);
+            open(fullpath, ((*mode == 'r')?O_RDONLY:O_WRONLY));
         f = fdopen(retcode, mode);
         }
     else{
